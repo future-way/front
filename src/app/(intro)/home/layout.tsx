06c0ab4621d7e539c4916dc4ac72fcc9df1faaa9
annkimm <zizicoo208@naver.com>
2024-12-15 08:38:44 +0900

    add home page background color

--- src/app/(intro)/home/layout.tsx
+++ src/app/(intro)/home/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="fex-col flex h-dvh flex-col text-center">
+    <div className="bg-home flex h-dvh flex-col text-center">
       <Header prevLink="" isShowBackBtn={false} width={25} />
       {children}
     </div>
