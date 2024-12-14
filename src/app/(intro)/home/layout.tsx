1c638665242018d4ea3ff21623504b46ab765147
annkimm <zizicoo208@naver.com>
2024-12-15 00:57:13 +0900

    test work for safari browser

--- src/app/(intro)/home/layout.tsx
+++ src/app/(intro)/home/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="fex-col flex h-screen text-center">
+    <div className="fex-col flex h-dvh flex-col text-center">
       <Header prevLink="" isShowBackBtn={false} width={25} />
       {children}
     </div>
